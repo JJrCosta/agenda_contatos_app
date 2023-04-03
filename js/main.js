2d8552bf83e9129c5806559e176ff038e7eaa9a3
JJrCosta <jrpercosta@gmail.com>
2023-04-03 00:06:00 -0300

    Feat: Add dinamic contact list

--- js/main.js
+++ js/main.js
@@ -1,17 +1,36 @@
-const atualizaForm = document.querySelector('form-add');
-const nomeContato  = document.getElementById('nome-contato');
-const phoneContato  = document.getElementById('phone');
+const listaNomesContatos = [];
+const listaTelContatos = [];
+let linhas = '';
 
-atualizaForm.addEventListener('submit', function (e) {
-    e.preventDefault()
-    alert(phoneContato.value)
+document.getElementById('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    criaLinha()
+    adicionaLinha()
 });
 
-function validaNome() {
+function criaLinha() {
+    const nomeContato = document.getElementById('nome-contato');
+    const phoneContato = document.getElementById('phone');
     
+    if (listaTelContatos.includes(phoneContato.value)) {
+        alert('Telefone já cadastrado.')
+    } else {
+        listaNomesContatos.push(nomeContato.value);
+        listaTelContatos.push(phoneContato.value);
+
+        let linha = '<tr>';
+        linha += `<td>${(nomeContato.value).trim()}</td>`;
+        linha += `<td>${phoneContato.value}</td>`;
+        linha += `<td><a id="tabela-icon" href="tel:+55${(phoneContato.value).split(' ').join('')}"><img src="./img/btn-icon.png" alt="Chamar" title="Chamar"></a></td>`;
+
+        linhas += linha;
+    }
+
+    nomeContato.value = '';
+    phoneContato.value = '';
 }
 
-function criaLinha() {
-    nomeContato.value 
-    phoneContato.value
+function adicionaLinha() {
+    const novaLinha = document.getElementById('tabela');
+    novaLinha.innerHTML = linhas;
 }
